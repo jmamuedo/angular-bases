58e14faf68f35f6f4d5c03c3eaecf0ea22e9faf4
JotaEme <jmamuedo@gmail.com>
2023-07-10 21:24:18 +0200

    Sección 5 completada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
 import { CounterModule } from './counter/components/counter.module';
+import { DbzModule } from './dbz/dbz.module';
 import { HeroesModule } from './heroes/heroes.module';
+
+import { AppComponent } from './app.component';
 
 
 @NgModule({
@@ -14,6 +16,7 @@ import { HeroesModule } from './heroes/heroes.module';
     BrowserModule,
     CounterModule,
     HeroesModule,
+    DbzModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
